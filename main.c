598b000646fe14dff58a1ccef570555a0d8b88cf
Ketchup Dev <120859934+usernameisrobert@users.noreply.github.com>
2026-06-04 00:37:52 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "raylib.h"
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+void UpdateDrawFrame(void) {
+    BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("so stinky", 190, 200, 20, LIGHTGRAY);
+    EndDrawing();
+}
+
+int main(void) {
+    InitWindow(800, 450, "so stinky"); //so stinky
+
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        UpdateDrawFrame();
+    }
+#endif
+
+    CloseWindow();
+    return 0;
+}
