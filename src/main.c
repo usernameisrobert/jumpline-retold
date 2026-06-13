3a77fae8ef37a4b00ce6c2ab81c5b7b481eed6cc
Levi <levidalrymple1221@gmail.com>
2026-06-13 18:26:24 -0500

    center it

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 #include <emscripten.h>
 #endif
 
+const int WIDTH = 800;
+const int HEIGHT = 450;
+
 Color BGblack;
 Texture2D logo;
 Vector2 logopos;
@@ -16,11 +19,11 @@ void UpdateDrawFrame() {
 }
 
 int main() {
-    InitWindow(800, 450, "Jumpline Retold"); //hey look its title
+    InitWindow(WIDTH, HEIGHT, "Jumpline Retold"); //hey look its title
     BGblack = GetColor(0x0F0F1FFF);
     logo = LoadTexture("assets/jumplineretoldsmall.png");
     SetTextureFilter(logo, TEXTURE_FILTER_BILINEAR);
-    logopos = (Vector2){ 50.0f, 50.0f };
+    logopos = (Vector2){ (WIDTH / 2.0f) - logo.width / 2.0, HEIGHT / 2.0f - logo.height / 2.0};
 
 #ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
