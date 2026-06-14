7420defdcacfd99d0b9f1ad8393df3a8c4902303
Ketchup Dev <120859934+usernameisrobert@users.noreply.github.com>
2026-06-13 22:11:10 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -20,7 +20,7 @@ void RenderDrawEmScripten() {
 }
 
 void UpdateFunction(float dt, Scene* scene) { // there, lazy dev, i did it, you mean-head
-    scene->player.vel.y += 1000 * dt;
+    scene->player.vel.y += 2000 * dt;
     scene->player.vel.x = scene->player.vel.x * 0.8;
     scene->player.pos.y += scene->player.vel.y * dt;
     scene->player.pos.x += scene->player.vel.x * dt;
@@ -31,7 +31,7 @@ void UpdateFunction(float dt, Scene* scene) { // there, lazy dev, i did it, you 
         isgrounded = true;
     }
     if (IsKeyPressed(KEY_SPACE) && isgrounded) {
-        scene->player.vel.y = -500;
+        scene->player.vel.y = -800;
     }
     if (IsKeyDown(KEY_A)) {
         scene->player.vel.x -= 100;
